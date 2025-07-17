4b09ff648ba676e63fda2d0765580348999ca5ec
Andre Velez <andrevelez151@hotmail.com>
2025-07-17 14:33:14 -0500

    estudiando node.js v3

--- 5.tests.js
+++ 5.tests.js
@@ -1,16 +1,15 @@
-const path = require("node:path");
 const fs = require("node:fs/promises");
 
 let folder = process.argv[2] ?? ".";
-
+console.log(folder);
 async function ls(folder) {
   try {
-    let files = await fs.readdir(folder);
-    console.log(files);
+    console.log(folder);
+    let show = await fs.readdir(folder);
+    console.log(show);
   } catch (error) {
     console.log("No se encontró el directorio");
     process.exit(1);
   }
 }
-
 ls(folder);
